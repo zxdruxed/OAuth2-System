013216f45bb641ab6c3b20d506e01fd431ab1ad1
zxdruxed <zxdruxed@gmail.com>
2024-11-23 10:10:21 +0300

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 import requests
 
+CLIENT_ID = YOUR_CLIENT_ID
+CLIENT_SECRET = YOUR_CLIENT_SECRET
+DOMAIN = 'emeraldermine.onpella.app'
+
+app.secret_key = 'my_secret_key'
+
 
 if __name__ == '__main__':
   app.run()
